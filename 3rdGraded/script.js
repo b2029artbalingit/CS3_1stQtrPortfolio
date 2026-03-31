dd8cab981c773fe5465cdee3ef9d439ba82cef55
b2029artbalingit <b2029artbalingit@pshs.edu.ph>
2026-03-31 15:12:07 +0800

    First Commit

--- 3rdGraded/script.js
+++ 3rdGraded/script.js
@@ -1,11 +1,13 @@
-function plotPoint() {
-    console.log(x0, y0, x, y) // checks the arguments passed to this function
+function plotPoint(x0, y0, x, y) {
+  const in1 = document.getElementbyId("in1");
+  const out1 = document.getElementbyId("out1");
+  console.log(x0, y0, x, y) // checks the arguments passed to this function 
     in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
     var point = document.createElement('div');  
     point.className = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
+    point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
     document.getElementById('coordinatePlane').appendChild(point);
 
     if (x > x0 && y > y0){
@@ -23,3 +25,4 @@ function plotPoint() {
     else{
       out1.innerHTML = "divisa"
     }
+  }
